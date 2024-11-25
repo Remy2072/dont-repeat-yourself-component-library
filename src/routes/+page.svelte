864eb13777f5d47a5175b64041dea52c40a36cc6
Remy Duivesteijn <remyd03@icloud.com>
2024-11-25 19:41:17 +0100

    ?

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,43 +1,42 @@
 <script>
-    import { Footer, Navigation, Heading } from "$lib";
+  import { Footer, Navigation, Heading } from "$lib";
 </script>
-  
-  <Navigation />
-  
-  <div class="container">
-    <Heading />
-  
-  </div>
-  
-  <Footer />
-  
-  <style>
+
+<Navigation />
+
+<div class="container">
+  <Heading />
+
+</div>
+
+<Footer />
+
+<style>
+  .container {
+    width: 100%;
+    background: var(--container-gradient);
+    border-radius: 24px;
+    padding: 0 2.188rem 0 2.188rem;
+    height: 100vh;
+  }
+
+  @media screen and (min-width: 768px) {
     .container {
-      width: 100%;
-      background: var(--container-gradient);
-      border-radius: 24px;
-      padding: 0 2.188rem 0 2.188rem;
-      height: 100vh;
+      border-radius: 30px 0 30px 30px;
+      padding: 0 3.125rem 0 3.125rem;
     }
-  
-    @media screen and (min-width: 768px) {
-      .container {
-        border-radius: 30px 0 30px 30px;
-        padding: 0 3.125rem 0 3.125rem;
-      }
+  }
+
+  @media screen and (min-width: 1024px) {
+    .container {
+      border-radius: 2.5rem;
+      padding: 0 6.25rem 0 6.25rem;
     }
-  
-    @media screen and (min-width: 1024px) {
-      .container {
-        border-radius: 2.5rem;
-        padding: 0 6.25rem 0 6.25rem;
-      }
+  }
+
+  @media screen and (min-width: 1440px) {
+    .container {
+      padding: 0 9.375rem 0 9.375rem;
     }
-  
-    @media screen and (min-width: 1440px) {
-      .container {
-        padding: 0 9.375rem 0 9.375rem;
-      }
-    }
-  </style>
-  
+  }
+</style>
